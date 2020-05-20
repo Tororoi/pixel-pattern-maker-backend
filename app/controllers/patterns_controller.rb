d6ddddac83e7dae1cabcf269914c48d78fce0db4
Tororoi <cantwell.tom@gmail.com>
2020-05-20 17:30:51 -0400

    disable admin mode

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,5 +1,5 @@
 class PatternsController < ApplicationController
-    skip_before_action :authorized, only: [:index, :destroy]
+    skip_before_action :authorized, only: [:index]
 
     def index 
         @patterns = Pattern.all
