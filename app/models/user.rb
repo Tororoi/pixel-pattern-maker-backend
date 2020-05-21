3f0832f9e5257beb8459dcd974dadcdaf73370cc
Tororoi <cantwell.tom@gmail.com>
2020-05-21 12:28:49 -0400

    changed user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true
     validates :username, uniqueness: { case_sensitive: false }
-    validates :username, format: { with: /[A-Za-z0-9]+/,
+    validates :username, format: { with: /\A[A-Za-z0-9]+\z/,
         message: "only allows letters and numbers" }
 
     has_many :patterns
